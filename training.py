60ecb72d41ee008bd6f0a83104f2e848504e0b19
Dimuth De Zoysa <dimuthsakya@protonmail.com>
2024-03-02 19:16:37 +0530

    Update training.py

--- training.py
+++ training.py
@@ -9,10 +9,11 @@ from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,Activation,Dropout
 from tensorflow.keras.optimizers import SGD
+#from tensorflow.keras.optimizers import SGD
 
 lematizer = WordNetLemmatizer()
 
-intents = json.loads(open(r'C:\Users\Asus\Documents\Python_Projects\AI_chatbot\intents.json').read())
+intents = json.loads(open('intents.json').read())
 
 words = []
 classes = []
@@ -34,28 +35,30 @@ words = sorted(set(words))
 
 classes = sorted(set(classes))
 
-pickle.dump(words, open('words.pkl','wb'))
-pickle.dump(words, open('classes.pkl','wb'))
+pickle.dump(words, open('words1.pk','wb'))
+pickle.dump(classes, open('classes.pkl','wb'))
 
-training = []
+training_bag = []
+training_output_row = []
 output_empty = [0] * len(classes)
 
 for document in documents:
-    bag = []
-    word_patterns = document[0]
-    word_patterns = [lematizer.lemmatize(word.lower()) for word in word_patterns]
-    for word in words:
-        bag.append(1) if word in word_patterns else bag.append(0)
+   bag = []
+   word_patterns = document[0]
+   word_patterns = [lematizer.lemmatize(word.lower()) for word in word_patterns]
+   for word in words:
+      bag.append(1) if word in word_patterns else bag.append(0)
 
-    output_row = list(output_empty)
-    output_row[classes.index(document[1])] = 1
-    training.append([bag,output_row])
+      output_row = list(output_empty)
+      output_row[classes.index(document[1])] = 1
+      training_bag.append(bag)
+      training_output_row.append(output_row)
+  
+training_bag = np.array(training_bag)
+training_output_row = np.array(training_output_row)
 
-random.shuffle(training)
-training = np.array(training)
-
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+from sklearn.utils import shuffle
+train_x, train_y = shuffle(training_bag, training_output_row, random_state=0)
 
 model = Sequential()
 model.add(Dense(128,input_shape=(len(train_x[0]),), activation='relu'))
@@ -64,9 +67,11 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-sgd = SGD(lr=0.01,decay=1e-6,momentum=0.9,nesterov=True)
+sgd = SGD(lr=0.01,momentum=0.9,nesterov=True)
 model.compile(loss='categorical_crossentropy',optimizer=sgd,metrics=['accuracy'])
 
-model.fit(np.array(train_x),np.array(train_y),epochs=200,batch_size=5,verbose=1)
-model.save('chatbot_model.model')
+#model.fit(np.array(train_x),np.array(train_y),epochs=200,batch_size=5,verbose=1)
+#model.save(r'C:\Users\Asus\Documents\Python_Projects\AI_chatbot\chatbot_model.model')
+hist = model.fit(np.array(train_x),np.array(train_y),epochs=200,batch_size=5,verbose=1)
+model.save(r'chatbotmodel2.h5',hist)
 print('Done')
